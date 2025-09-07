fd3a1fb21266af59db144ba7d6278d9e312a0d13
sampaiolucasdev <lsampaio32@gmail.com>
2025-09-07 10:13:40 -0300

    api funcional

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,24 @@
 import * as http from 'http';
+import {
+  getFilterEpisodes,
+  getListEpisodes,
+} from "./controllers/podcasts-controller";
 
-const server = http.createServer((req:http.IncomingMessage, res:http.ServerResponse) =>{})
+const server = http.createServer(
+  async (req: http.IncomingMessage, res: http.ServerResponse) => {
+    //queryString
+    const [baseURL, queryString] = req.url?.split("?") ?? ["", ""];
+
+    if (req.method === "GET" && baseURL === "/api/list") {
+      await getListEpisodes(req, res);
+    }
+    if (req.method === "GET" && baseURL === "/api/episode") {
+      await getFilterEpisodes(req, res);
+    }
+  }
+);
+
+const port = process.env.PORT;
+server.listen(port, () => {
+  console.log(`servidor iniciado na porta ${port}`);
+});
---
+++ src/services/list-episodes-service.ts
@@ -0,0 +1,9 @@
+import { repositoryPodcast } from "../repositories/podcasts-repository";
+
+
+
+
+export const serviceListEpisodes = async ()=>{
+  const data = await repositoryPodcast()
+    return data;
+}
